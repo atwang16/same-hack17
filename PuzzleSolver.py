89e48ad31fdb7e36eb594ad8b4dcd6e69943c608
Miles Dai <milesdai@mit.edu>
2017-09-16 18:16:12 -0400

    Inserted some test code in PuzzleSolver.py

--- PuzzleSolver.py
+++ PuzzleSolver.py
@@ -6,6 +6,21 @@ class PuzzleSolver():
 	def __init__(self):
 		self.pieces = list()
 
+	def get_edges_test(self):
+		"""Takes an image and gives an array same dimension as image that has 1 if
+		edge, 0 if not for each pixel."""
+		img = cv2.imread('samples/realpuzzle.jpg')
+
+		lowThreshold = 30
+		ratio = 3
+		kernel_size = 3
+		gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+		img = cv2.GaussianBlur(gray,(3,3),6)
+		img = cv2.Canny(img,lowThreshold,lowThreshold*ratio,apertureSize = kernel_size)
+		cv2.imshow('test', img)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+
 	def import_pieces(self, path_to_pieces):
 		pass
 
