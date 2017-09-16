5dc9ca361d9406b2c06fec4e1c658641537bd254
Miles Dai <milesdai@mit.edu>
2017-09-16 17:00:31 -0400

    Added PuzzleSolver class and main template

---
+++ PuzzleSolver.py
@@ -0,0 +1,17 @@
+import numpy as np 
+import cv2
+import sys
+
+class PuzzleSolver():
+	def __init__(self):
+		self.pieces = list()
+
+	def import_pieces(self, path_to_pieces):
+		pass
+
+	def preprocess_pieces(self):
+		"""Detect the edges of the puzzle piece and remove background"""
+		pass
+
+if __name__ == '__main__':
+	pass
---
+++ main.py
@@ -0,0 +1,5 @@
+from PuzzleSolver import *
+
+if __name__ == '__main__':
+	solver = PuzzleSolver()
+	solver.import_pieces('../PuzzlePieces')
