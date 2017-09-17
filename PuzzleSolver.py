c4e2feeff83eed0fc5b5643dc4436a40252c5629
Shahul Alam <alam.shahul@gmail.com>
2017-09-17 01:22:13 -0400

    Added rudiments of matching algorithm.

--- PuzzleSolver.py
+++ PuzzleSolver.py
@@ -23,30 +23,43 @@ def get_edges_test():
 def match_colors_test():
 	img1 = cv2.imread('samples/sample1/NWpiece.jpeg')
 	height1, width1 = len(img1), len(img1[0])
-	img2 = cv2.imread('samples/sample1/SWpiece.jpeg')
+	img2 = cv2.imread('samples/sample1/SEpiece.jpeg')
         height2, width2 = len(img2), len(img2[0])
 
+	images = (cv2.imread('samples/sample1/NWpiece.jpeg'), cv2.imread('samples/sample1/NEpiece.jpeg'), cv2.imread('samples/sample1/SWpiece.jpeg'), cv2.imread('samples/sample1/SEpiece.jpeg'))
+	
+	edgesfinal=[np.reshape(bin_colors(img1[height1-1]), -1), np.reshape(bin_colors(img1[0]), -1), np.reshape(bin_colors(img1[:, width1 - 1]), -1), np.reshape(bin_colors(img1[:, 0]), -1), np.reshape(bin_colors(img2[height2-1]), -1), np.reshape(bin_colors(img2[0]), -1), np.reshape(bin_colors(img2[:, width2 - 1]), -1), np.reshape(bin_colors(img2[:, 0]), -1)]
+	sixteenedges = [[0 for t in range(4*len(images))] for s in range(4*len(images))]
+	for i in range(len(images)):
+		sixteenedges[4*i] = np.reshape(bin_colors(images[i][0]), -1)
+		sixteenedges[4*i+1] = np.reshape(bin_colors(images[i][:, len(images[i][0]) - 1]), -1)
+		sixteenedges[4*i+2] = np.reshape(bin_colors(images[i][len(images[i]) - 1]), -1)
+		sixteenedges[4*i+3] = np.reshape(bin_colors(images[i][:, 0]), -1)
+	print(sixteenedges)
 
-	def bin_colors(pixels):
-		bintransform=np.zeros((15, 15, 15))
-		size=len(pixels)
-		for i in range(size):
-			pixel=pixels[i]
-			blue=int(math.ceil(pixel[0]/float(17))) - 1 
-			green=int(math.ceil(pixel[1]/float(17))) - 1
-			red=int(math.ceil(pixel[2]/float(17))) -1
-#			if pixel[0] > 254  or pixel[1] > 254 or pixel[2] > 254:
-#				print(blue, green, red)
-#				print(bintransform[blue, green, red])
-			bintransform[blue, green, red] += 1
-		return bintransform
-	bin_colors(img1[height1-1])	
-	edges=((bin_colors(img1[height1-1]), 1),(bin_colors(img2[height2-1]), 2),(bin_colors(img1[:, width1-1]), 1),(bin_colors(img2[:, width2-1]), 2),(bin_colors(img1[0]), 1),(bin_colors(img2[0]), 2), (bin_colors(img1[:, 0]), 1),(bin_colors(img2[:, 0]), 2))
-#	print(edges[0][0])
-	print(distance.cdist([np.reshape(edges[0][0], -1)], [np.reshape(edges[1][0], -1)]))
-	edges1=[np.reshape(bin_colors(img1[height1-1]), -1), np.reshape(bin_colors(img1[0]), -1), np.reshape(bin_colors(img1[:, width1 - 1]), -1), np.reshape(bin_colors(img1[:, 0]), -1)]
-	edges2=[np.reshape(bin_colors(img2[height2-1]), -1), np.reshape(bin_colors(img2[0]), -1), np.reshape(bin_colors(img2[:, width2 - 1]), -1), np.reshape(bin_colors(img2[:, 0]), -1)]
-	print(distance.cdist(edges1, edges2))
+
+
+	distances= distance.cdist(sixteenedges, sixteenedges)
+	# Set impossible edge matchups to infinite
+	for i in range(len(distances)):
+		number=i%4
+		distances[i][i]= np.inf
+		for j in range(number):
+			distances[i][i-j-1] = np.inf
+			distances[i-j-1][i] = np.inf
+				
+#	print(distances)
+	steps = [0 for i in range(len(distances))]
+	stepnum=0
+	while (np.min(distances) != np.inf):
+		indices = np.unravel_index(np.argmin(distances), distances.shape)
+		print(indices)
+		steps[stepnum] = indices
+		stepnum += 1
+		distances[indices[0]] = np.inf
+		distances[:, indices[0]] = np.inf
+		distances[indices[1]] = np.inf
+		distances[:, indices[1]] = np.inf
 
 class PuzzleSolver():
 	def __init__(self):
@@ -56,17 +69,16 @@ class PuzzleSolver():
 		pass
 
 	def get_front_binary_image(self, img):
-    """
-    TODO: write description
+	"""
+	TODO: write description
 
-    :param img: an image with a white background
-    :return:
-    """
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert from color to grayscale
-    img_gray_gauss = cv2.GaussianBlur(img_gray, (5, 5), 0) # apply Gaussian blur
-    th, img_gray_thresh = cv2.threshold(img_gray_gauss, 245, 255, cv2.THRESH_BINARY) # threshold image so relevant part becomes black
-    return cv2.bitwise_not(img_gray_thresh)
-
+	:param img: an image with a white background
+	:return:
+	"""
+	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert from color to grayscale
+	img_gray_gauss = cv2.GaussianBlur(img_gray, (5, 5), 0) # apply Gaussian blur
+	th, img_gray_thresh = cv2.threshold(img_gray_gauss, 245, 255, cv2.THRESH_BINARY) # threshold image so relevant part becomes black
+ 	return cv2.bitwise_not(img_gray_thresh)
 
 	def get_back_binary_image(self, img):
 	    """
