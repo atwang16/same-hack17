7c524f409f62c1d57010face97486deda6d12f6a
Miles Dai <milesdai@mit.edu>
2017-09-16 22:15:39 -0400

    Implemented get_corners()

--- PuzzleSolver.py
+++ PuzzleSolver.py
@@ -5,24 +5,34 @@ import sys
 class PuzzleSolver():
 	def __init__(self):
 		self.pieces = list()
-
-	def get_edges_test(self):
-		"""Takes an image and gives an array same dimension as image that has 1 if
-		edge, 0 if not for each pixel."""
-		img = cv2.imread('samples/realpuzzle.jpg')
-
-		lowThreshold = 30
-		ratio = 3
-		kernel_size = 3
-		gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-		img = cv2.GaussianBlur(gray,(3,3),6)
-		img = cv2.Canny(img,lowThreshold,lowThreshold*ratio,apertureSize = kernel_size)
-		cv2.imshow('test', img)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
-
+	
 	def import_pieces(self, path_to_pieces):
 		pass
+
+	def get_corners(self, img, visualize=False):
+	"""Detects the corners of the puzzle piece.
+	
+	RETURN
+	A list of python lists of the x and y coordinates
+	"""
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = np.float32(gray)
+    gray = cv2.GaussianBlur(gray,(3,3),0)
+    corners = cv2.goodFeaturesToTrack(gray, 4, 0.01, 10)
+    corners = np.int0(corners)
+    
+    # Clean up output
+    corner_list = list()
+    for i in corners:
+        x, y = i.ravel()
+        corner_list.append([x, y])
+        if visualize:
+        	cv2.circle(img, (x, y), 3, 255, -1)
+	if visualize:
+	    cv2.imshow("image", img)
+	    cv2.waitKey(0)
+	    cv2.destroyAllWindows()
+    return corner_list
 
 	def preprocess_pieces(self):
 		"""Detect the edges of the puzzle piece and remove background"""
